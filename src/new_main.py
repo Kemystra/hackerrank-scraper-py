7b3bd91cb6d8dd70daeaf6d588f766234aa9c8bc
Kemystra <programmer.coleslaw981@slmail.me>
2024-11-22 16:28:26 +0800

    Replaced Selenium with requests

--- src/new_main.py
+++ src/new_main.py
@@ -1,7 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from requests.utils import cookiejar_from_dict
 import os
@@ -17,33 +13,15 @@ delay = 5  # seconds
 mx_retries = 3
 
 
-def get_submission_ids(contest_name, challenge_id):
-    session = requests.session()
-    cookies = dict({
-        token_name: token_value
-    })
-    cookies = cookiejar_from_dict(cookies)
-    session.cookies.update(cookies)
-
+def get_submission_ids(contest_name, challenge_id, session):
     url = f'https://www.hackerrank.com/rest/contests/{contest_name}/judge_submissions/?offset=0&limit=1&challenge_id={challenge_id}'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-        'Content-Type': 'application/json'
-    }
 
-    response = session.get(
-        url,
-        headers=headers
-    )
+    response = session.get(url)
 
-    total = response.json()['total']
     ids = []
 
     url = f'https://www.hackerrank.com/rest/contests/{contest_name}/judge_submissions/?offset=0&limit=1000000&challenge_id={challenge_id}'
-    response = session.get(
-        url,
-        headers=headers
-    )
+    response = session.get(url)
 
     submissions = response.json()['models']
     for j in submissions:
@@ -55,55 +33,30 @@ def get_submission_ids(contest_name, challenge_id):
     return ids
 
 
-def scrape_submissions(id, driver):
-    url = f'https://www.hackerrank.com/contests/{contest_name}/challenges/{challenge_id}/submissions/code/{id}'
-    driver.get(url)
-    retry = 1
-
-    while (retry <= mx_retries):
-        time.sleep(delay)
-        try:
-            username = driver.find_element(By.CSS_SELECTOR, '.alert-info .bold').text
-
-            challenge_name = driver.find_element(By.CSS_SELECTOR, '.hr_tour-challenge-name').text
-            challenge_name = [i.lower() for i in challenge_name if i.isalpha() or i == ' ']
-            challenge_name = ''.join(challenge_name).replace(' ', '_')
-
-            lines = driver.find_elements(By.CSS_SELECTOR, '.CodeMirror-line')
-
-            codes = []
-            for line in lines:
-                codes.append(line.text)
-
-            os.makedirs(f'./{challenge_name}/', exist_ok=True)
-            with open(f'./{challenge_name}/{username}.txt', 'w') as f:
-                f.write('\n'.join(codes))
-            break
-        except Exception as e:
-            print(f'Attempt #{retry}: Error occured at page {url}')
-
-        retry += 1
+def scrape_submissions(id, session):
+    pass
 
 
 if __name__ == "__main__":
-    submission_ids = get_submission_ids(contest_name, challenge_id)
-    print(f"Scraped a total of {len(submission_ids)} submission IDs.")
+    session = requests.session()
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    cookies = dict({
+        token_name: token_value
+    })
 
-    driver.get('https://www.hackerrank.com/')
-    cookie = {
-        'name': token_name,
-        'value': token_value,
-        'domain': 'hackerrank.com',
-        'path': '/',
+    cookies = cookiejar_from_dict(cookies)
+    session.cookies.update(cookies)
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'Content-Type': 'application/json'
     }
 
-    driver.add_cookie(cookie)
+    session.headers.update(headers)
+    submission_ids = get_submission_ids(contest_name, challenge_id, session)
+    print(f"Scraped a total of {len(submission_ids)} submission IDs.")
 
-    driver.refresh()
+    print(submission_ids)
 
     for i in submission_ids:
-        scrape_submissions(i, driver)
-
-    driver.quit()
+        scrape_submissions(i, session)
